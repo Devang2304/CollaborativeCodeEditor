3ba45ebea8095711be14bdcea7326b0abc0b23e2
Swastik Verma <swastikverma308@gmail.com>
2023-10-29 07:19:19 +0530

    started adding comments

--- CodeEditor-Backend/src/main/java/com/CodeEditor/CodeEditorBackend/model/Project.java
+++ CodeEditor-Backend/src/main/java/com/CodeEditor/CodeEditorBackend/model/Project.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
+//This is the model/schema of the project
 @Data
 @Builder
 @NoArgsConstructor
